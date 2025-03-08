532494896ef0b2ed9dc2fb34feaeb87e0303f60b
Akshay B <akshay2642005@gmail.com>
2025-03-08 08:56:20 +0530

    (feat)latest_verison

--- wezterm/lua/appeareance.lua
+++ wezterm/lua/appeareance.lua
@@ -1,36 +1,21 @@
 local M = {}
-local wez = require "wezterm"
 M.apply_to_config = function(c)
-  c.color_scheme = "Cloud (terminal.sexy)"
-  --c.color_scheme = 'kanagawabones'
-  --c.color_scheme = "Rosé Pine Moon (Gogh)"
-  c.default_cursor_style = "SteadyBar"
-  c.inactive_pane_hsb = { brightness = 0.9 }
-  c.window_decorations = "RESIZE"
-  c.show_new_tab_button_in_tab_bar = false
-  c.enable_tab_bar = false
-  c.show_tabs_in_tab_bar = false
-  c.use_fancy_tab_bar = false
-  --c.window_background_opacity = 0.8
-  c.colors = {
-    background = 'rgb(0, 0, 0,65%)',
-    cursor_border = "#bea3c7",
-    cursor_bg = "#bea3c7",
-    tab_bar = {
-      background = 'rgb(0, 0, 0, 65%)', -- dark purple
-    }
-  }
-
-  c.window_frame = {
-    font = wez.font({ family = "IosevkaTerm Nerd Font", weight = "Bold" }),
-    active_titlebar_bg = "#0e0e12",
-  }
-  c.window_padding = {
-    left = '1cell',
-    right = '1cell',
-    top = '0.28cell',
-    bottom = '0.08cell',
-  }
+	c.color_scheme = "Black Metal (Gorgoroth) (base16)"
+	c.default_cursor_style = "SteadyBar"
+	c.inactive_pane_hsb = { brightness = 0.9 }
+	c.window_decorations = "RESIZE"
+	c.show_new_tab_button_in_tab_bar = false
+	c.enable_tab_bar = false
+	c.show_tabs_in_tab_bar = false
+	c.use_fancy_tab_bar = false
+	c.colors = {
+		background = "rgb(0,0,0, 68%)",
+		cursor_border = "#ffffff",
+		cursor_bg = "#ffffff",
+		tab_bar = {
+			background = "rgb(0,0,0, 68%)",
+		},
+	}
 end
 
 return M
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,49 +1,26 @@
 --- My Wezterm config file
 
-
-local wez = require "wezterm"
-local utils = require "lua.utils"
-local mappings = require "lua.mappings"
-local appearance = require "lua.appeareance"
---local bar = wez.plugin.require "https://github.com/adriankarlen/bar.wezterm"
-
-
+local wez = require("wezterm")
+local mappings = require("lua.mappings")
+local appearance = require("lua.appeareance")
 local c = {}
-
-
 if wez.config_builder then
-  c = wez.config_builder()
+	c = wez.config_builder()
 end
-
-
-c.font = wez.font_with_fallback {
-  family = 'IosevkaTerm Nerd Font',
-  stretch = 'Expanded',
-  weight = 'Regular',
-}
+c.front_end = "OpenGL"
+c.animation_fps = 1
+c.font = wez.font_with_fallback({
+	--family = 'IosevkaTerm Nerd Font',
+	family = "JetBrainsMono Nerd Font",
+	stretch = "Expanded",
+	weight = "Regular",
+})
+c.max_fps = 240
 c.cell_width = 0.9
-c.font_size = 15
-c.default_prog = utils.is_windows and {
-  "C:\\Program Files\\PowerShell\\7\\pwsh.exe", "-NoLogo"
-}
-c.adjust_window_size_when_changing_font_size = false
-c.audible_bell = "Disabled"
-c.scrollback_lines = 1000
-c.default_workspace = "main"
-c.status_update_interval = 1000
---[[ c.window_background_opacity = 0.99 ]]
-
-
-
-
+c.font_size = 14
+c.default_prog = { "pwsh","-NoLogo" }
 -- appearance
 appearance.apply_to_config(c)
-
-
 -- keys
 mappings.apply_to_config(c)
-
--- bar
---bar.apply_to_config(c, { enabled_modules = { hostname = false } })
-
 return c
