6b4f8f51dae63d443504be8c8b89c791c6ffac7a
Akshay B <akshay2642005@gmail.com>
2025-03-08 08:48:56 +0530

    (feat)latest_verison

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,9 +3,8 @@
 
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 Import-Module posh-git
-$omp_config = Join-Path $PSScriptRoot ".\takuya.omp.json"
+$omp_config = Join-Path $PSScriptRoot "./clean-detailed.omp.json"
 oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
-
 
 Import-Module -Name Terminal-Icons
 Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
@@ -15,7 +14,6 @@ Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
 Set-PSReadLineOption -PredictionSource History
-
 Set-PSReadlineOption -Colors @{
     Type = "DarkCyan"
     Member = "Gray"
@@ -28,50 +26,45 @@ Set-PSReadlineOption -Colors @{
     Variable = "Magenta"
     Parameter = "Gray"
 }
-
 # Fzf
 Import-Module PSFzf
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
-
 # Env
 $env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
 $env:desktop = "C:\Users\M S I\OneDrive\Desktop"
 $ENV:SSH_AUTH_SOCK = $null
+$env:EDITOR = "nvim"
+#$env:NVIM_APPNAME = ".nv-ide"
+$env:VIRTUAL_ENV -replace ".*\\", ""
+
 
 # Alias
 Set-Alias -Name neofetch -Value fastfetch
-Set-Alias -Name gh -Value auto-git
+Set-Alias -Name agt -Value auto-git
 Set-Alias -Name lg -Value lazygit
+Set-Alias -Name lds -Value lazydocker
 Set-Alias -Name vim -Value nvim
 Set-Alias g git
 Set-Alias grep findstr
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
 # Utilities
+function dock {
+    docker desktop start
+}
+
 function which ($command) {
   Get-Command -Name $command -ErrorAction SilentlyContinue |
     Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
 
 Set-Alias ytdlp 'C:\Users\M S I\Documents\PowerShell\Scripts\youtube-dl Easy Script.ps1'
-
-Set-Alias lvim 'C:\Users\aksha\.local\bin\lvim.ps1'
-
-Set-Alias lvim 'C:\Users\M S I\.local\bin\lvim.ps1'
-
 Set-Alias -Name yz -Value yazi 
+Set-Alias -Name vi -Value arch
 
-function coder {
-    Start-Process -FilePath "C:\Users\M S I\AppData\Local\Programs\Ollama\Ollama.exe" -ArgumentList "run", "qwen2.5-coder:7b-instruct-q8_0" -NoNewWindow -Wait 
-}
-
-function gemma {
-    Start-Process -FilePath "C:\Users\M S I\AppData\Local\Programs\Ollama\Ollama.exe" -ArgumentList "run", "gemma2:9b" -NoNewWindow -Wait 
-}
-#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
 
 Import-Module -Name Microsoft.WinGet.CommandNotFound
-#f45873b3-b655-43a6-b217-97c00aa0db58
+
 function fx { nvim (fzf --preview "bat --style=numbers --color=always {1}") }
 function fz { neovide (fzf --preview "bat --style=numbers --color=always {1}") }
 function fs { fzf --preview "bat --style=numbers --color=always {1}"}
@@ -100,7 +93,6 @@ function mkdir {
         [Parameter(Mandatory = $true)]
         [string]$FolderName
     )
-
     try {
         # Resolve the full path to avoid confusion
         $fullPath = Resolve-Path -Path $FolderName -ErrorAction SilentlyContinue
@@ -111,7 +103,6 @@ function mkdir {
 
         # Change into the directory
         Set-Location -Path $fullPath
-
         # Confirm the operation
         Write-Output "Successfully created and entered the directory: $(Get-Location)"
     } catch {
@@ -120,5 +111,4 @@ function mkdir {
     }
 }
 
-
-
+clear
