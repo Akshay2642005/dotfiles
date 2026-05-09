d15bf129e29990c57cc580d5648c77a642fb9909
Akshay B <akshay2642005@gmail.com>
2026-05-09 18:23:16 +0530

    update dot files

--- wezterm/lua/appeareance.lua
+++ wezterm/lua/appeareance.lua
@@ -2,14 +2,49 @@ local M = {}
 M.apply_to_config = function(c)
   -- c.color_scheme = "Black Metal (Gorgoroth) (base16)"
   c.color_scheme = "Black Metal (Gorgoroth) (base16)"
-  c.window_decorations = "RESIZE"
-  c.enable_tab_bar = false
+  c.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+  c.enable_tab_bar = true
+  c.use_fancy_tab_bar = true
   c.window_background_opacity = 1
   c.colors = {
     background = "rgb(0,0,0, 100%)",
     cursor_border = "#ffffff",
     cursor_bg = "#ffffff",
+    foreground = 'silver',
+    selection_fg = 'black',
+    split = '#444444',
+    tab_bar = {
+      active_tab = {
+        bg_color = '#000000',
+        fg_color = '#808080',
+        intensity = 'Half',
+        strikethrough = false,
+      },
+      inactive_tab = {
+        bg_color = '#000000',
+        fg_color = '#808080',
+      },
 
+      inactive_tab_hover = {
+        bg_color = '#000000',
+        fg_color = '#808080',
+        italic = true,
+      },
+
+      new_tab = {
+        bg_color = '#000000',
+        fg_color = '#808080',
+      },
+      new_tab_hover = {
+        bg_color = '#3b3052',
+        fg_color = '#909090',
+        italic = true,
+      },
+    },
+  }
+  c.window_frame = {
+    active_titlebar_bg = 'rgb(0,0,0)',
+    inactive_titlebar_bg = 'rgb(0,0,0)',
   }
 end
 
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,42 +1,56 @@
+---@diagnostic disable: unused-local
 --- My Wezterm config file
 local wez = require("wezterm")
 local mappings = require("lua.mappings")
 local appearance = require("lua.appeareance")
-local tab = require("lua.tabline")
-
+local menu = require("lua.menu")
 local c = {}
 if wez.config_builder then
   c = wez.config_builder()
 end
-c.font = wez.font_with_fallback({
-  family = "JetBrainsMono Nerd Font",
-  stretch = "Expanded",
-  weight = "Regular",
-})
--- c.window_padding = {
---   left = 10,
---   right = 10,
---   top = 10,
---   bottom = 10,
--- }
-c.use_resize_increments = true
-
+--
 -- c.font = wez.font_with_fallback({
---   "IosevkaTerm Nerd Font",
+--   "GeistMono Nerd Font",
 --   weight = "Regular",
 -- })
+c.font = wez.font("GeistMono Nerd Font")
+
+c.window_padding = {
+  left = "5px",
+  right = "5px",
+  top = "0px",
+  bottom = "1px",
+}
+
+c.use_resize_increments = true
 c.max_fps = 240
 c.cell_width = 0.9
-c.font_size = 12
+c.line_height = 1.0
+c.font_size = 13
+c.line_height = 1
 c.front_end = "OpenGL"
-c.default_prog = { "pwsh" }
--- c.default_prog = { "C:\\Program Files\\Git\\bin\\bash.exe" }
--- c.default_prog = { "wsl" }
+c.term = "xterm-256"
+c.default_prog = { 'pwsh.exe', '-NoLogo' }
+
+-- tab name change
+wez.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+  local cwd = tab.active_pane.current_working_dir
+  if cwd then
+    -- Show only the last directory component
+    local path = cwd.file_path
+    local home = wez.home_dir
+    path = path:gsub(home, '~')             -- replace home with ~
+    path = path:match('([^/]+)/*$') or path -- grab last segment
+    return path
+  end
+  return tab.active_pane.title
+end)
+
 -- appearance
 appearance.apply_to_config(c)
 -- keys
 mappings.apply_to_config(c)
--- tabline
-tab.apply_to_config(c)
+
+menu.apply_to_config(c)
 
 return c
